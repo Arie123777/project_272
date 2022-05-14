7fdcf5aa1cb5896ae849ae2ba2dfdfb904bcea9f
Kasin-ks <ivylee950@gmail.com>
2022-05-14 22:47:21 +0800

    Link the react with index.html

--- src/component/App.js
+++ src/component/App.js
@@ -8,56 +8,24 @@
  * He Yauhi 1155143159
  */
 
-import React from "react";
-import { HashRouter as Router, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-import UserLoginScreen from "../panel/user/UserLoginScreen";
-import UserHomeScreen from "../panel/user/UserHomeScreen";
-import UserMapScreen from "../panel/user/UserMapScreen";
-import UserFavouriteScreen from "../panel/user/UserFavouriteScreen";
-import AboutScreen from "../panel/user/AboutScreen";
+const RedirectToHome = () => {
+  useEffect(() => {
+    window.location = "/0index.html";
+  }, []);
 
-import AdminUserCRUD from "../panel/admin/AdminUserCRUD";
-import AdminLocCRUD from "../panel/admin/AdminLocCRUD";
-import AdminUpdate from "../panel/admin/AdminUpdate";
-
-import { Provider as GameProvider } from "../context/GameContext";
-import { Provider as AuthProvider } from "../context/AuthContext";
-import Resolver from "../panel/Resolver";
+  return <></>;
+};
 
 const App = () => {
   return (
-    <GameProvider>
-      <AuthProvider>
-        <Router>
-          <Switch>
-            <Resolver path="/" exact component={Resolver} />
-
-            <Resolver path="/user/login" exact component={UserLoginScreen} />
-            <Resolver path="/user/home" exact component={UserHomeScreen} />
-            <Resolver path="/user/map/:loc_id?" component={UserMapScreen} />
-            <Resolver
-              path="/user/favourite"
-              exact
-              component={UserFavouriteScreen}
-            />
-            <Resolver path="/user/about" exact component={AboutScreen} />
-
-            <Resolver
-              path="/admin/manage/user"
-              exact
-              component={AdminUserCRUD}
-            />
-            <Resolver
-              path="/admin/manage/location"
-              exact
-              component={AdminLocCRUD}
-            />
-            <Resolver path="/admin/update" exact component={AdminUpdate} />
-          </Switch>
-        </Router>
-      </AuthProvider>
-    </GameProvider>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={RedirectToHome} />
+      </Switch>
+    </Router>
   );
 };
 
